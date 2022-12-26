231422c527d8005b0b36c73a12ae6f3f6319ca2b
chunw100 <116131880+chunw100@users.noreply.github.com>
2022-12-26 20:26:46 +0800

    sss

---
+++ 電腦垃圾清理.bat
@@ -0,0 +1,19 @@
+@echo off
+(cd /d "%~dp0")&&(NET FILE||(powershell start-process -FilePath '%0' -verb runas)&&(exit /B)) >NUL 2>&1
+cd %temp%
+cd ..
+rd temp /s /q 2>nul & cd temp
+cd C:\Windows\SoftwareDistribution
+rd Download /s /q & cd Download
+cd C:\Windows 
+rd /s /q Prefetch & md Prefetch
+wsreset.exe
+ping 8.8.8.8 -n 1
+taskkill /f /im winstore.app.exe
+ipconfig /flushdns >nul
+cls
+echo 請點選清理系統檔案，並全部勾選，在按清理
+start cleanmgr.exe
+echo 完成
+echo 按任意鍵離開
+pause >nul
